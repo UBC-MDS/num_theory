2e83894db87697cef2c1fe5ba47ab5606c049ae7
Thamerd <thamerd@student.ubc.ca>
2025-01-08 15:37:18 -0800

    Added docstring for get_primes

---
+++ get_primes.py
@@ -0,0 +1,17 @@
+def get_primes(n: int) -> list:
+    '''
+    This function takes an integer n and returns the list of all primes under n.
+
+    Input:
+    integer n. 
+
+    Output:
+    List of integers containing prime numbers under n.
+
+    Example:
+    get_primes(14) -> [2, 5, 7, 11, 13]
+
+#primesieve already does this
+    '''
+    pass
+    
